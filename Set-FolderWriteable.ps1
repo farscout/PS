75f8a6674a343f6aa9e00285cd96c8a31aa9e0ce
farscout <farscout@gmail.com>
2016-01-02 18:34:11 +1000

    Script to make files and folders below a folder writeable. Typically used for getting files from tfs then building with a script.

---
+++ Set-FolderWriteable.ps1
@@ -0,0 +1,11 @@
+<#
+.DESCRIPTION
+Example command for forcing all files in a folder and below to
+to be writeable. Typically used when getting source files from
+tfs and modifying during a build without read-only causing build errors
+.PARAMETER folder
+path to recurse into and below making all files writeable
+#>
+param($folder)
+
+gci -Path $folder -Recurse -attributes readonly | % { $_.IsReadonly = $false }
